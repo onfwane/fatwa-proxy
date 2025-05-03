23ab759208f9d89dc492fa4e3eaecdd42bbab19d
onfwane <W.Alhasaseen@gju.edu.jo>
2025-05-03 18:42:17 +0300

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "microsoft/phi-4-reasoning-plus:free", // نموذج جيد ومجاني ويدعم العربية
+        model: "opengvlab/internvl3-14b:free", // نموذج جيد ومجاني ويدعم العربية
         messages
       })
     });
